f4b8e94b5f60d6430256b47b72e01e0baf23ab9e
Heba-Webdev23 <jtvonline@yahoo.com>
2024-11-19 23:19:33 +0300

    feat: update customers table migration

---
+++ Repositories/Migrations/20241119201809_UpdateCustomers.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCustomers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Customers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"),
+                column: "Password",
+                value: "$2a$11$QwvfxSYtIiN1Gb3IfAewOOCwtVis2.69j4PFeCg4b.rBavXIASR1G");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("c9d4c053-49b6-410c-bc78-2d54a9991870"),
+                column: "Password",
+                value: "$2a$11$f8Cx4TJ5JWmn93cvOaSAXO/Vm31R9CcdwrhYe/HZsAyZpklraNM3S");
+        }
+    }
+}
