e3494c68b56c806c90f0d6279f3edc3af9f41363
Heba-Webdev23 <jtvonline@yahoo.com>
2024-11-19 23:46:17 +0300

    feat: creat customer controller

--- Presentation/Controllers/CustomersController.cs
+++ Presentation/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 namespace Presentation.Controllers;
 
 [Route("api/customers")]
@@ -8,6 +9,17 @@ public class CustomersController : ControllerBase
 {
     private readonly IServiceManager _service;
     public CustomersController(IServiceManager service) => _service = service;
+
+    [HttpPost]
+    public IActionResult CreateCustomer([FromBody] CreateCustomerRequestDto customer)
+    {
+        if (customer is null)
+            return BadRequest("Request body can not be empty");
+
+        var createdCustomer = _service.CustomerService.CreateCustomer(customer);
+
+        return CreatedAtRoute("CustomerById", new { id = createdCustomer.Id }, createdCustomer);
+    }
 
     [HttpGet]
     public IActionResult GetCustomers()
