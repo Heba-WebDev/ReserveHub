d8537f6a56dc8a933e0e90c3ddaa763621643559
Heba-Webdev23 <jtvonline@yahoo.com>
2024-11-25 14:39:28 +0300

    feat: add optional user-id and remove password

--- Shared/DataTransferObjects/CreateCustomerRequestDto.cs
+++ Shared/DataTransferObjects/CreateCustomerRequestDto.cs
@@ -12,12 +12,7 @@ public class CreateCustomerRequestDto
     [Required(ErrorMessage = "Email is required")]
     [EmailAddress(ErrorMessage = "Invalid email format")]
     public required string Email { get; set; }
-
-    [Required(ErrorMessage = "Password is required")]
-    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$",
-        ErrorMessage = "Password must be at least 8 characters long, contain at least one letter, one number, and one special character")]
-    public required string Password { get; set; }
-
+    public Guid? UserId { get; set; }
     public string? PhoneNumber { get; set; }
     public string? Address { get; set; }
 }
