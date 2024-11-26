f8a73647e7ae16437546dfa81cb9763505e4ed58
Heba-Webdev23 <jtvonline@yahoo.com>
2024-11-26 13:29:16 +0300

    feat: users service interface

--- Service.Contracts/IServiceManager.cs
+++ Service.Contracts/IServiceManager.cs
@@ -2,6 +2,7 @@ namespace Service.Contracts;
 
 public interface IServiceManager
 {
+    IUserService UserService { get; }
     ICustomerService CustomerService { get; }
     IRoomService RoomService { get; }
     IAmenityService AmenityService { get; }
