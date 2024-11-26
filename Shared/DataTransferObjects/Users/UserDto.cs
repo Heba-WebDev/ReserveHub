436df0a617f7bf6fe20e7b42b412f270e88c08be
Heba-Webdev23 <jtvonline@yahoo.com>
2024-11-26 13:30:05 +0300

    feat: users dto

---
+++ Shared/DataTransferObjects/Users/UserDto.cs
@@ -0,0 +1,11 @@
+namespace Shared.DataTransferObjects.Users;
+
+public record UserDto
+{
+    public Guid Id { get; init; }
+    public string? FirstName { get; init; }
+    public string? LastName { get; init; }
+    public string? Email { get; init; }
+    public string? PhoneNumber { get; init; }
+    public string? Address { get; init; }
+}
