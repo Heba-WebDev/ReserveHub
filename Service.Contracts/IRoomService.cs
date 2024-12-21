ceb15a2c5fe6faaf8f2c1db8fe9c801a11dfc455
Heba-Webdev23 <jtvonline@yahoo.com>
2024-12-21 22:06:37 +0300

    feat: create rooms service interface

--- Service.Contracts/IRoomService.cs
+++ Service.Contracts/IRoomService.cs
@@ -1,6 +1,7 @@
+using Shared.DataTransferObjects.Rooms;
 namespace Service.Contracts;
 
 public interface IRoomService
 {
-
+    Task<RoomResponseDto> CreateRoom(CreateRoomRequestDto createRoomRequestDto);
 }
