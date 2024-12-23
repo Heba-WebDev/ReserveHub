db37955afde2c68010650a22ee2033ba6a7e2d19
Heba-Webdev23 <jtvonline@yahoo.com>
2024-12-23 17:08:23 +0300

    feat: get-all rooms

--- Service.Contracts/IRoomService.cs
+++ Service.Contracts/IRoomService.cs
@@ -1,7 +1,10 @@
+using Entities.Models;
 using Shared.DataTransferObjects.Rooms;
+using Shared.RequestFeatures;
 namespace Service.Contracts;
 
 public interface IRoomService
 {
     Task<RoomResponseDto> CreateRoom(CreateRoomRequestDto createRoomRequestDto);
+    Task<(IEnumerable<RoomResponseDto>? rooms, MetaData metaData)> GetAllRooms(RoomParameters roomParameters, bool trackChanges);
 }
