da56a8026f96599015f2c00876f29143d509f446
Heba-Webdev23 <jtvonline@yahoo.com>
2025-01-06 12:59:28 +0300

    feat: orderby parameter

--- Shared/RequestFeatures/CustomerParameters.cs
+++ Shared/RequestFeatures/CustomerParameters.cs
@@ -2,5 +2,6 @@ namespace Shared.RequestFeatures;
 
 public class CustomerParameters : RequestParameters
 {
+    public CustomerParameters() => OrderBy = "FirstName";
     public string? SearchTerm { get; set; }
 }
