905c29b2c17902f642642ceb750bd805c32d730a
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-01 18:53:43 +0300

    chore: update create user reutrn type

--- Service.Contracts/IUserService.cs
+++ Service.Contracts/IUserService.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity;
 using Shared.DataTransferObjects.Users;
 namespace Service.Contracts;
 
 public interface IUserService
 {
-    Task<UserDto> CreateUser(CreateUserRequestDto user);
+    Task<IdentityResult> CreateUser(CreateUserRequestDto user);
     Task<UserDto> GetUserById(Guid userId, bool trackChanges);
     Task UpdateUser(Guid userId, UpdateUserRequestDto user, bool trackChanges);
 }
