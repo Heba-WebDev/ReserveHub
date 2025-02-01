0cf310bb6702623ea70b8a2101b718f5de734cd6
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-01 18:51:26 +0300

    chore: inherti from identity user

--- Entities/Models/User.cs
+++ Entities/Models/User.cs
@@ -1,13 +1,10 @@
+using Microsoft.AspNetCore.Identity;
 namespace Entities.Models;
 
-public class User
+public class User : IdentityUser
 {
-    public Guid Id { get; set; }
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
-    public required string Email { get; set; }
-    public required string Password { get; set; }
-    public string? PhoneNumber { get; set; }
     public string? Address { get; set; }
     public Customer? Customers { get; set; }
 }
