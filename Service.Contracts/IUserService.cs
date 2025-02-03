31192a82b183f92db141bce0f48cff8e2d8ba6fb
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-03 18:03:57 +0300

    feat: validate user and create token

--- Service.Contracts/IUserService.cs
+++ Service.Contracts/IUserService.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Identity;
+using Shared.DataTransferObjects.Auth;
 using Shared.DataTransferObjects.Users;
 namespace Service.Contracts;
 
 public interface IUserService
 {
     Task<IdentityResult> CreateUser(CreateUserRequestDto user);
+    Task<bool> ValidateUser(LoginDto dto);
+    Task<string> CreateToken();
     Task<UserDto> GetUserById(Guid userId, bool trackChanges);
     Task UpdateUser(Guid userId, UpdateUserRequestDto user, bool trackChanges);
 }
