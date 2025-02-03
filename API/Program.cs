f5132001ae3e25689621e9d2f0bce7c90745947e
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-03 18:03:04 +0300

    feat: jwt configuration

--- API/Program.cs
+++ API/Program.cs
@@ -48,7 +48,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication();
 builder.Services.ConfigureIdentity();
-
+builder.Services.ConfigureJWT(builder.Configuration);
 var app = builder.Build();
 app.ConfigureExceptionHandler();
 // Configure the HTTP request pipeline.
