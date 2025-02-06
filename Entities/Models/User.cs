f80a13eca663a787b945ea8b5d162f628568011f
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-06 17:55:36 +0300

    feat: refresh token and refresh token expiry time

--- Entities/Models/User.cs
+++ Entities/Models/User.cs
@@ -6,5 +6,7 @@ public class User : IdentityUser
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public string? Address { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiryTime { get; set; }
     public Customer? Customers { get; set; }
 }
