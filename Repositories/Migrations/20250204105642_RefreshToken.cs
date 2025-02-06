c5d22439ee0bb3626a9f300be87dfd79cc72af83
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-06 17:59:26 +0300

    feat: roles migration

---
+++ Repositories/Migrations/20250204105642_RefreshToken.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "462ea0f4-3e47-49f9-a6f3-ecbef7994d33");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "65e1e108-236a-40f2-9a0a-d3b58a887ef1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "b7cb22b1-8e9a-4bf5-876f-a9aefca3a28f", null, "Customer", "Customer" },
+                    { "cb5bbe89-ea83-4d8d-83bd-7322eea69d1e", null, "Admin", "ADMIN" },
+                    { "e1233289-6053-41ca-b635-376248f99c06", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b7cb22b1-8e9a-4bf5-876f-a9aefca3a28f");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cb5bbe89-ea83-4d8d-83bd-7322eea69d1e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e1233289-6053-41ca-b635-376248f99c06");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "462ea0f4-3e47-49f9-a6f3-ecbef7994d33", null, "User", "USER" },
+                    { "65e1e108-236a-40f2-9a0a-d3b58a887ef1", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
