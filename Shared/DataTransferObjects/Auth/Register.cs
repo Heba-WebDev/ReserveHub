cbf00dff6618ffa27fb210128a715a80984c074d
Heba-Webdev23 <jtvonline@yahoo.com>
2025-02-15 00:19:32 +0300

    feat: register dto

---
+++ Shared/DataTransferObjects/Auth/Register.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+namespace Shared.DataTransferObjects.Auth;
+public class RegisterDto
+{
+    [Required(ErrorMessage = "First name is required")]
+    public required string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Last name is required")]
+    public required string LastName { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid email format")]
+    public required string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$",
+        ErrorMessage = "Password must be at least 8 characters long, contain at least one letter, one number, and one special character")]
+    public required string Password { get; set; }
+
+    public string? PhoneNumber { get; set; }
+
+    public string? Address { get; set; }
+}
