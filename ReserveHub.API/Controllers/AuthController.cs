d8a3ae4c1c7fff9a3a50eb6221785b1e9230f5b9
Heba-Webdev23 <jtvonline@yahoo.com>
2025-07-18 15:28:02 +0300

    feat: register

---
+++ ReserveHub.API/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using ReserveHub.Application.DTOs;
+using ReserveHub.Application.DTOs.Auth;
+using ReserveHub.Application.Services.Contracts;
+namespace ReserveHub.API.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly IServiceManager _manger;
+
+    public AuthController(IServiceManager manager)
+    {
+        _manger = manager;
+    }
+
+    [HttpPost("register")]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(BasedResponseDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register(RegisterDto dto)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var result = await _manger.AuthService.Register(dto);
+
+        return StatusCode(StatusCodes.Status201Created, result);
+    }
+}
---
+++ ReserveHub.Infrastructure/Services/AuthService.cs
@@ -0,0 +1,62 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using ReserveHub.Application.DTOs;
+using ReserveHub.Application.DTOs.Auth;
+using ReserveHub.Application.Services.Contracts;
+using ReserveHub.Domain.Entities;
+using ReserveHub.Domain.Repositories;
+using ReserveHub.Infrastructure.Configurations;
+namespace ReserveHub.Infrastructure.Services;
+
+public class AuthService : IAuthService
+{
+    public readonly IRepositoryManager _repository;
+    public readonly IMapper _mapper;
+    private readonly UserManager<User> _userManager;
+    private readonly IOptions<JwtConfiguration> _configuration;
+    private readonly JwtConfiguration _jwtConfiguration;
+    private User? _user;
+    public AuthService(IRepositoryManager repository, IMapper mapper, UserManager<User> userManager, IOptions<JwtConfiguration> configuration)
+    {
+        _repository = repository;
+        _mapper = mapper;
+        _userManager = userManager;
+        _configuration = configuration;
+        _jwtConfiguration = _configuration.Value;
+    }
+
+    public async Task<BasedResponseDto> Register(RegisterDto dto)
+    {
+        var user = _mapper.Map<User>(dto);
+        var result = await _userManager.CreateAsync(user, dto.Password);
+
+        if (!result.Succeeded)
+        {
+            return new BasedResponseDto()
+            {
+                Status = false,
+                Message = "Registration failed",
+                Data = result.Errors.Select(e => e.Description)
+            };
+        }
+
+        await _userManager.AddToRoleAsync(user, "Customer");
+
+        return new BasedResponseDto()
+        {
+            Status = true,
+            Message = "User successfully registered",
+        };
+    }
+
+    public Task<BasedResponseDto> Login(LoginDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BasedResponseDto> RefreshToken(TokenDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
