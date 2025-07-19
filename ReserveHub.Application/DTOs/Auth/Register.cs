71dfd0058c52a989e795ccf90191a2929fe62eef
heba-webdev <jtvonline@yahoo.com>
2025-07-19 13:14:09 +0300

    fix: simpler password validation

--- ReserveHub.Application/DTOs/Auth/Register.cs
+++ ReserveHub.Application/DTOs/Auth/Register.cs
@@ -11,10 +11,11 @@ public class RegisterDto
     [EmailAddress(ErrorMessage = "Invalid email format")]
     public required string Email { get; set; }
     [Required(ErrorMessage = "Password is required")]
-    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$",
-        ErrorMessage = "Password must be at least 8 characters long, contain at least one letter, one number, and one special character")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+    [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).*$",
+        ErrorMessage = "Password must contain at least one letter, one number, and one special character")]
     public required string Password { get; set; }
-    
+
     [Phone(ErrorMessage = "Invalid phone number format")]
     public string? PhoneNumber { get; set; }
     public string? Address { get; set; }
