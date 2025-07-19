0f043e0aad6ff2c4ed3ea3664a856951cee321e2
heba-webdev <jtvonline@yahoo.com>
2025-07-19 13:03:21 +0300

    chore: inject dependecies

--- ReserveHub.API/Program.cs
+++ ReserveHub.API/Program.cs
@@ -1,4 +1,5 @@
 using ReserveHub.API.Extensions;
+using ReserveHub.Infrastructure.Mappings;
 using ReserveHub.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +8,12 @@ builder.Services.AddControllers();
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
 .AddJsonFile("appsettings.dev.json", optional: true, reloadOnChange: true)
 .AddEnvironmentVariables();
-builder.Services.ConfigurePostgresSqlContext(builder.Configuration);
+builder.Services.AddAutoMapper(cfg => { }, typeof(MappingProfile).Assembly);
+builder.Services.ConfigurePostgresSqlContext(builder.Configuration);builder.Services.AddSwaggerGen();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.ConfigureIdentity();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
