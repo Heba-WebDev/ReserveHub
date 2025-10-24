e0d5866678a7ac8152c1ab12b4406003530f1b4e
heba-webdev <jtvonline@yahoo.com>
2025-10-24 14:26:46 +0300

    feat: google auth service

--- ReserveHub.Application/Services.Contracts/IServiceManager.cs
+++ ReserveHub.Application/Services.Contracts/IServiceManager.cs
@@ -3,4 +3,5 @@ namespace ReserveHub.Application.Services.Contracts;
 public interface IServiceManager
 {
     IAuthService AuthService { get; }
+    IGoogleAuthService GoogleAuthService { get;  }
 }
