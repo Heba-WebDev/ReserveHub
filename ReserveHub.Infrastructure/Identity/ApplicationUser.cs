015f053f4c8d2a212f763aa3a4456f878697863a
heba-webdev <jtvonline@yahoo.com>
2025-10-24 14:45:26 +0300

    chore: mark google id as personal data

--- ReserveHub.Infrastructure/Identity/ApplicationUser.cs
+++ ReserveHub.Infrastructure/Identity/ApplicationUser.cs
@@ -7,6 +7,7 @@ public class ApplicationUser : IdentityUser
     public required string FullName { get; set; }
     public string? RefreshToken { get; set; }
     public DateTime RefreshTokenExpiryTime { get; set; }
+    [PersonalData]
     public string? GoogleId { get; set; }
     public ICollection<Hotel> OwnedHotels { get; set; } = new List<Hotel>();
     public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
